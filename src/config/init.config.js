7f29bb8170a77244273e899522280e3a1cf878ba
itisaambiental <it@isaambiental.com>
2024-10-02 16:57:22 -0600

    Deploy

--- src/config/init.config.js
+++ src/config/init.config.js
@@ -1,6 +1,6 @@
 import UserService from '../services/User.service.js'
 import UserRepository from '../repositories/User.repository.js'
-import { ADMIN_GMAIL, ADMIN_NAME, ADMIN_ROLE } from './variables.config.js'
+import { ADMIN_GMAIL, ADMIN_NAME, ADMIN_ROLE, ADMIN_GMAIL_SUPPORT, ADMIN_ROLE_SUPPORT, ADMIN_NAME_SUPPORT } from './variables.config.js'
 import ErrorUtils from '../utils/Error.js'
 import fs from 'fs'
 import path from 'path'
@@ -12,28 +12,47 @@ const __dirname = path.dirname(__filename)
 export const initializeAdmin = async () => {
   try {
     const existingAdmin = await UserRepository.findByGmail(ADMIN_GMAIL)
-    if (existingAdmin) {
-      return
+    if (!existingAdmin) {
+      const adminData = {
+        name: ADMIN_NAME,
+        gmail: ADMIN_GMAIL,
+        roleId: ADMIN_ROLE
+      }
+
+      const profilePicturePath = path.join(__dirname, '../resources/foto.jpg')
+      const profilePicture = {
+        originalname: 'foto.jpg',
+        mimetype: 'image/jpeg',
+        buffer: fs.readFileSync(profilePicturePath)
+      }
+
+      await UserService.registerUser(adminData, profilePicture)
+      console.log('Admin user created successfully.')
     }
 
-    const adminData = {
-      name: ADMIN_NAME,
-      gmail: ADMIN_GMAIL,
-      roleId: ADMIN_ROLE
-    }
+    // Verifica si ya existe el usuario de soporte
+    const existingSupportUser = await UserRepository.findByGmail(ADMIN_GMAIL_SUPPORT)
+    if (!existingSupportUser) {
+      const supportData = {
+        name: ADMIN_NAME_SUPPORT,
+        gmail: ADMIN_GMAIL_SUPPORT,
+        roleId: ADMIN_ROLE_SUPPORT
+      }
 
-    const profilePicturePath = path.join(__dirname, '../resources/foto.jpg')
-    const profilePicture = {
-      name: 'foto.jpg',
-      mimetype: 'image/jpeg',
-      buffer: fs.readFileSync(profilePicturePath)
-    }
+      const profilePicturePathSupport = path.join(__dirname, '../resources/foto_support.jpg') // Usar otra imagen si es necesario
+      const profilePictureSupport = {
+        originalname: 'foto_support.jpg',
+        mimetype: 'image/jpeg',
+        buffer: fs.readFileSync(profilePicturePathSupport)
+      }
 
-    await UserService.registerUser(adminData, profilePicture)
+      await UserService.registerUser(supportData, profilePictureSupport)
+      console.log('Support user created successfully.')
+    }
   } catch (error) {
     if (error instanceof ErrorUtils) {
-      console.error('Error during admin initialization:', error.message)
+      console.error('Error during admin or support initialization:', error.message)
     }
-    throw new ErrorUtils(500, 'Failed to initialize admin', error)
+    throw new ErrorUtils(500, 'Failed to initialize admin or support user', error)
   }
 }
