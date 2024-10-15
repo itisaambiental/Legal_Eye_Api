dc1bfd26af45f56ac49eb681c9421d50a37d9852
itisaambiental <it@isaambiental.com>
2024-10-15 16:32:27 -0600

    Testing Backend

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import { app } from './app.js'
 import { PORT } from './config/variables.config.js'
 import { initializeAdmin } from './config/init.config.js'
 
+const DEFAULT_PORT = 3000
+const serverPort = PORT || DEFAULT_PORT
+
 if (process.env.NODE_ENV !== 'test') {
   initializeAdmin()
 }
-const server = app.listen(PORT, () => {
-  console.log('Server running on port', PORT)
+
+const server = app.listen(serverPort, () => {
+  console.log('Server running on port', serverPort)
 })
 
 export { app, server }
