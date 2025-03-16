4b65c90867bdd10fadfa1d3de0949709e0dc1727
Alfredo <alfredocastellanoula.14@gmail.com>
2025-03-15 18:21:47 -0600

    Fix enviroments secrets

--- src/config/redis.config.js
+++ src/config/redis.config.js
@@ -1,17 +1,8 @@
-/**
- * Exports the Redis configuration object based on the current environment.
- * Adjusts Redis settings for production, development, and test environments.
- */
-
 import {
   REDIS_PASS,
   REDIS_USER,
   REDIS_HOST,
   REDIS_PORT,
-  REDIS_PASS_DEV,
-  REDIS_USER_DEV,
-  REDIS_HOST_DEV,
-  REDIS_PORT_DEV,
   REDIS_PASS_TEST,
   REDIS_USER_TEST,
   REDIS_HOST_TEST,
@@ -20,13 +11,7 @@ import {
 } from './variables.config.js'
 
 /**
- * Determine if the environment is production.
- * @type {boolean}
- */
-const isProduction = NODE_ENV === 'production'
-
-/**
- * Determine if the environment is test.
+ * Determine if the application is running in the test environment
  * @type {boolean}
  */
 const isTest = NODE_ENV === 'test'
@@ -36,10 +21,10 @@ const isTest = NODE_ENV === 'test'
  * @type {Object}
  */
 export const redisConfig = {
-  host: isProduction ? REDIS_HOST : isTest ? REDIS_HOST_TEST : REDIS_HOST_DEV,
-  port: isProduction ? REDIS_PORT : isTest ? REDIS_PORT_TEST : REDIS_PORT_DEV,
-  password: isProduction ? REDIS_PASS : isTest ? REDIS_PASS_TEST : REDIS_PASS_DEV,
-  username: isProduction ? REDIS_USER : isTest ? REDIS_USER_TEST : REDIS_USER_DEV,
+  host: isTest ? REDIS_HOST_TEST : REDIS_HOST,
+  port: isTest ? REDIS_PORT_TEST : REDIS_PORT,
+  password: isTest ? REDIS_PASS_TEST : REDIS_PASS,
+  username: isTest ? REDIS_USER_TEST : REDIS_USER,
   db: 0,
   connectTimeout: 10000
 }
