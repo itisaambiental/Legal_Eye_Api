d69645272f0b18c3deaeceee2e326ba9a4815d09
Alfredo <alfredocastellanoula.14@gmail.com>
2025-03-17 16:01:06 -0600

    Implementing Migration to Database

--- src/config/redis.config.js
+++ src/config/redis.config.js
@@ -3,6 +3,10 @@ import {
   REDIS_USER,
   REDIS_HOST,
   REDIS_PORT,
+  REDIS_PASS_DEV,
+  REDIS_USER_DEV,
+  REDIS_HOST_DEV,
+  REDIS_PORT_DEV,
   REDIS_PASS_TEST,
   REDIS_USER_TEST,
   REDIS_HOST_TEST,
@@ -11,20 +15,41 @@ import {
 } from './variables.config.js'
 
 /**
- * Determine if the application is running in the test environment
- * @type {boolean}
+ * Defines the Redis configuration settings based on the current environment.
+ * Each environment (development, test, production) has its own Redis parameters.
+ * @type {Object}
  */
-const isTest = NODE_ENV === 'test'
+const config = {
+  development: {
+    host: REDIS_HOST_DEV,
+    port: REDIS_PORT_DEV,
+    password: REDIS_PASS_DEV,
+    username: REDIS_USER_DEV
+  },
+  test: {
+    host: REDIS_HOST_TEST,
+    port: REDIS_PORT_TEST,
+    password: REDIS_PASS_TEST,
+    username: REDIS_USER_TEST
+  },
+  production: {
+    host: REDIS_HOST,
+    port: REDIS_PORT,
+    password: REDIS_PASS,
+    username: REDIS_USER
+  }
+}
 
 /**
- * The Redis configuration settings.
+ * Exports the Redis configuration object based on the current environment.
+ * This configuration is used to initialize Redis clients for caching or pub/sub.
  * @type {Object}
  */
 export const redisConfig = {
-  host: isTest ? REDIS_HOST_TEST : REDIS_HOST,
-  port: isTest ? REDIS_PORT_TEST : REDIS_PORT,
-  password: isTest ? REDIS_PASS_TEST : REDIS_PASS,
-  username: isTest ? REDIS_USER_TEST : REDIS_USER,
+  host: config[NODE_ENV].host,
+  port: config[NODE_ENV].port,
+  password: config[NODE_ENV].password,
+  username: config[NODE_ENV].username,
   db: 0,
   connectTimeout: 10000
 }
